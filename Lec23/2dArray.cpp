418390f044f0043b1ff385d9b68f16f87b81b0ee
Anmol Sharma <anmolsharma2902@gmail.com>
2024-03-13 22:55:56 +0530

    13 Mar 2024 10:55pm

--- Lec23/2dArray.cpp
+++ Lec23/2dArray.cpp
@@ -6,5 +6,36 @@ using namespace std;
 int main(){
 
     int arr[3][4];
+    
+    //taking input from row
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cin>>arr[row][col];
+        }
+    }
 
+    //printing array row wise
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cout<<arr[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<endl;
+
+    //printing array col wise
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            cout<<arr[col][row]<<" ";
+        }
+        cout<<endl;
+    }   
 }
